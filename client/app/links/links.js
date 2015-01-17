f7ef447e0be6cb70a413ed3c831946473131d94f
James Kolonusz <jamesak@gmail.com>
2015-01-16 16:00:41 -0800

    Add data property on scope and getLinks method.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,18 @@
-angular.module('shortly.links', [])
+angular.module('shortly.links', []) //['ui.bootstrap']
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+
+  $scope.data = {
+
+  };
+
+
+  $scope.getLinks = function(){
+    return "asdf";
+  };
+
+  //$scope.init(getLinks);
+
 });
+
